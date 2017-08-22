401c38a5dc44b5ca4e035dad4c8e68c110920309
Chris den Arend <chris.denarend@quby.com>
2017-08-22 09:26:10 +0200

    Added question for name and question about current status of user

--- ChesterChatbot/Dialogs/RootDialog.cs
+++ ChesterChatbot/Dialogs/RootDialog.cs
@@ -23,9 +23,42 @@ namespace ChesterChatbot.Dialogs
             int length = (activity.Text ?? string.Empty).Length;
 
             // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            await context.PostAsync($"Hi, I'm Chester and I'll support your ambition to learn how to build a chatbot!");
 
-            context.Wait(MessageReceivedAsync);
+            context.Call(new NameDialog(), NameDialogResumeAfter);
+        }
+
+        private async Task NameDialogResumeAfter(IDialogContext context, IAwaitable<string> result)
+        {
+            var name = result.GetAwaiter().GetResult();
+            context.UserData.SetValue("name", name);
+
+            context.Call(new IntroDialog(), IntroDialogResumeAfter);
+        }
+
+        private async Task IntroDialogResumeAfter(IDialogContext context, IAwaitable<string> result)
+        {
+            try
+            {
+                switch (result.GetAwaiter().GetResult())
+                {
+                    case "My first time":
+                        context.Call(new IntroDialog(), IntroDialogResumeAfter);
+                        break;
+                    case "Already made some progress":
+                        context.Call(new IntroDialog(), IntroDialogResumeAfter);
+                        break;
+                    default:
+                        context.Call(new IntroDialog(), IntroDialogResumeAfter);
+                        break;
+                }
+            }
+            catch (TooManyAttemptsException)
+            {
+                //await context.PostAsync("Ik begrijp je helaas niet. Laten we het opnieuw proberen.");
+                await context.PostAsync("I don't understand, let's try again.");
+                await StartAsync(context);
+            }
         }
     }
 }
